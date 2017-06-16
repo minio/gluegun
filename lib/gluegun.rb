438dca7cd8805027b65bfd930b919f6176df4b38
Ersan <ersan.bozduman@gmail.com>
2017-06-16 12:30:31 -0700

    Adds comment and updates .gitignore file

--- lib/gluegun.rb
+++ lib/gluegun.rb
@@ -12,11 +12,16 @@ module Gluegun
     def self.gluegun_generate_index(site_config_file)
       dest_path = 'www'
       html_file = 'index.html'
-      partial_erb_arr =['lib/index.erb', 'lib/_header.erb', 'lib/_sidebar.erb', 'lib/_footer.erb']
+      partial_erb_arr =['lib/index.erb', 'lib/_header.erb',
+                        'lib/_sidebar.erb', 'lib/_footer.erb']
       @site_map = YAML.load(open(site_config_file).read)
       FileUtils.mkdir_p(dest_path) unless File.exist?(dest_path)
       File.open(File.join(dest_path, html_file), "w+") do |f|
         partial_erb_arr.each do |partial_erb|
+          # Set nil to "-" to activate "<%-" and "-%>" characters
+          # for non-printing lines in erb file.
+          # This will prevent extraneous newlines and indentations
+          # in the generated html file.
           f.puts(ERB.new(File.read(partial_erb), nil, '-').result(binding))
         end
       end
